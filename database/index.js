e0bc2106b04dbe4f90533e1236fbb94dbe0a6556
Jessica Thompson <jthompson0670@gmail.com>
2017-12-31 11:20:49 -0800

    mysql functions

--- database/index.js
+++ database/index.js
@@ -1,19 +1,42 @@
 const mysql = require('mysql');
 
-//initiate connection
+//create connection
 let connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: ''
 });
 
+//connect
 connection.connect(function(err) {
 	if(err) {
 		console.log(err)
 	} else {
 		console.log('connected!')
 	}
-})
+});
+
+let insertMovie = (data, cb) => {
+	let query = 'INSERT INTO movies (title, watched) values (?, ?)';
+	connection.query(query, [...data], (err) => {
+		if(err) {
+			cb(err)
+		} else {
+			cb(null)
+		}
+	})
+};
+
+let selectAll = (cb) => {
+	let query = 'SELECT * FROM movies';
+	connection.query(query, (err, results) => {
+		if(err) {
+			cb(err)
+		} else {
+			cb(results)
+		}
+	});
+}
 
 module.exports = {
 	selectAll: selectAll,
